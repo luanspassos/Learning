15543de8f981f4ed947415e717f907ec99071dae
Luan Passos <lpassos@americastg.com>
2024-10-24 17:52:37 -0300

    Tasks 1, 2, 3, 4

--- Learning/Program.cs
+++ Learning/Program.cs
@@ -4,24 +4,24 @@
     {
         private static void DoJob()
         {
-            Thread.Sleep(1000);
             for (int i = 0; i < 100; i++)
             {
                 Console.WriteLine(i);
             }
+            Thread.Sleep(2000);
         }
         static void Main(string[] args)
         {
             Task t = new Task(DoJob);
             t.Start();
+            t.Wait();
 
-            Task t2 = Task.Run(() =>
-            {
-                for (int i = 0; i < 100; i++)
-                {
-                    Console.WriteLine(i);
-                }
-            });
+            Task t2 = Task.Run(() => DoJob());
+            t2.Wait();
+
+            Task.Run(() => DoJob()).Wait();
+
+            Task.Factory.StartNew(DoJob);
 
             Console.ReadKey();
         }
