7939c91a02dadc1d35165205fe109d67213e6fce
Luan Passos <lpassos@americastg.com>
2024-10-24 17:31:32 -0300

    print 0 to 99

--- Learning/Program.cs
+++ Learning/Program.cs
@@ -2,9 +2,16 @@
 {
     internal class Program
     {
+        private static void DoJob()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DoJob();
         }
     }
 }
